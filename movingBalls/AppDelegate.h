40d26cd7708ac88d70a46e44ba2f584af333e653
Muthu Krishnan <niyatam@Muthu-Krishnan.local>
2015-08-12 11:13:49 +0530

    Initial Commit

---
+++ movingBalls/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  movingBalls
+//
+//  Created by Muthu Krishnan on 12/08/15.
+//  Copyright Muthu Krishnan 2015. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ movingBalls/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  movingBalls
+//
+//  Created by Muthu Krishnan on 12/08/15.
+//  Copyright Muthu Krishnan 2015. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
